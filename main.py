b91586feb1830fc13c7175ce9037c4938a06872c
Alyssa Kucharyski <adkucharyski@gmail.com>
2024-02-01 15:52:55 -0700

    decoded email body successfully!

--- main.py
+++ main.py
@@ -4,6 +4,8 @@ from google.oauth2.credentials import Credentials
 from google_auth_oauthlib.flow import InstalledAppFlow
 from googleapiclient.discovery import build
 from googleapiclient.errors import HttpError
+import base64 
+from bs4 import BeautifulSoup
 
 # If modifying these scopes, delete the file token.json.
 SCOPES = ["https://www.googleapis.com/auth/gmail.readonly"]
@@ -38,15 +40,25 @@ def main():
     results = service.users().labels().list(userId="me").execute()
     labels = results.get("labels", [])
 
-    message = service.users().messages().list(userId="me").execute()
-    messages = message.get("messages", [])
+     # request a list of all the messages  
+    result = service.users().messages().list(maxResults=2, userId='me').execute() 
+    messages = result.get('messages') 
+    for msg in messages: 
+        # Get the message from its id 
+        txt = service.users().messages().get(userId='me', id=msg['id']).execute() 
+        payload = txt['payload']
+        
+        parts = payload.get('parts')[0]
+        data = parts['body']['data']
 
-    if not messages:
-      print("No messages found.")
-      return
-    print("Messages:")  
-    for message in messages:
-      print(message["id"])
+        data = data.replace("-","+").replace("_","/") 
+        decoded_data = base64.b64decode(data) 
+        soup = BeautifulSoup(decoded_data , "lxml") 
+        body = soup.body() 
+        print("Message: ", body) 
+        print('\n') 
+        # Use try-except to avoid any Errors 
+       
 
     if not labels:
       print("No labels found.")
@@ -58,7 +70,6 @@ def main():
   except HttpError as error:
     # TODO(developer) - Handle errors from gmail API.
     print(f"An error occurred: {error}")
-
 
 if __name__ == "__main__":
   main()
