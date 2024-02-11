a8bcaa53aeff60482fcb2ce6dcdd114dcf03c1aa
Alyssa Kucharyski <adkucharyski@gmail.com>
2024-02-10 17:46:35 -0700

    HAHAHHA FIGURED IT OUT parts[0]['parts][0]['body']['data']

--- main.py
+++ main.py
@@ -16,8 +16,9 @@ SCOPES = ["https://www.googleapis.com/auth/gmail.readonly"]
 
 # database connection 
 myclient = pymongo.MongoClient(os.getenv('MONGO_URI'))
-ordersCollection = myclient["Orders"]
-itemsCollection = myclient["Items"]
+# print(myclient.list_database_names())
+# ordersCollection = myclient["Orders"]
+# itemsCollection = myclient["Items"]
 
 
 def main():
@@ -44,7 +45,7 @@ def main():
     # Call the Gmail API
     service = build("gmail", "v1", credentials=creds)
      # request a list of all the messages  
-    result = service.users().messages().list(maxResults=4, userId='me').execute() 
+    result = service.users().messages().list(maxResults=1, userId='me').execute() 
     messages = result.get('messages') 
     for msg in messages: 
         # Get the message from its id 
@@ -52,9 +53,10 @@ def main():
 
         # get the payload from the email message
         payload = txt['payload']
-
+        parts = payload.get('parts', [])
+        data = parts[0]['parts'][0]['body']['data']
         # get the headers from the payload | this returns a list of headers to loop through
-        headers = payload.get('headers') 
+        headers = payload['headers']
 
         # loop through the headers and get the Subject aka the order number which will be the ID
         for header in headers:
@@ -63,51 +65,52 @@ def main():
             orderNumber = subject[subject.find("Order")+len("Order "): subject.find("confirmed")]
             print(orderNumber)
         
-        # get the data from the body of the email
-        parts = payload.get('parts')[0]
-        if parts is not None:
-          # data = parts['body']['data']
-          if 'data' in parts['body']:
-            data = parts['body']['data']
-          else:
-            print("Data key is not in the body dictionary")
-            continue
-        else:
-          print("Parts is None")
-          continue
-        
-        # decode the data
+
         data = data.replace("-","+").replace("_","/") 
         decoded_data = base64.b64decode(data) 
         soup = BeautifulSoup(decoded_data , "lxml") 
-        body = str(soup.findAll('p'))
+        text = str(soup.body()) 
         keyWord = "Order summary"
-        # res=str(body)[str(body).find(keyWord)+len(keyWord):]
+        # res=text[text.find(keyWord)+len(keyWord):]
         # find the order summary
-        orderSummary = body[str(body).find(keyWord)+len(keyWord):str(body).find("Customer information")]
+        orderSummary = text[text.find(keyWord)+len(keyWord):text.find("Customer information")]
 
         # if the order summary has a discount
         # if discount -> pull the discount and save it into a variable
         # else - > go straight to subtotal
         if orderSummary.find("Discount"):
           # get the discount and save it into a variable
-          discount = orderSummary[orderSummary.find("Discount")+len("Discount"):orderSummary.find("Subtotal")]
-          print(discount)
+              discount = orderSummary[orderSummary.find("Discount")+len("Discount"):orderSummary.find("Subtotal")]
+              print(discount)
 
-          # get the items and save them into a variable
-          itemList = str(body)[str(body).find(keyWord)+len(keyWord):str(body).find("Discount")]
-          print(itemList)
+          # get the items and save  them into a variable
+              itemList = text[text.find(keyWord)+len(keyWord):text.find("Discount")]
+              print(itemList)
         else:
-          print("No discount")
+              print("No discount")
           # get the items and save them into a variable
-          itemList = str(body)[str(body).find(keyWord)+len(keyWord):str(body).find("Subtotal")]
+        
+        itemList = text[text.find(keyWord)+len(keyWord):text.find("Subtotal")]
 
           #get the information to insert into the order database
-          total = body[body.find("Total")+len("Total"):body.find("Shipping")]
-          subtotal = body[body.find("Subtotal")+len("Subtotal"):body.find("Customer Information")]  
-          shipping = body[body.find("Shipping")+len("Shipping"):body.find("Total")]
-          myDict = {"Total": total, "Subtotal": subtotal, "Shipping": shipping}
-          print(myDict)
+        total = text[text.find("Total")+len("Total"):text.find("Customer information")]
+        subtotal = text[text.find("Subtotal")+len("Subtotal"):text.find("Shipping")]  
+        shipping = text[text.find("Shipping")+len("Shipping"):text.find("Total")]
+          # data = parts['body']['data']
+        print("Total: ", total, "Subtotal: ", subtotal, "Shipping: ", shipping)
+        # get the data from the body of the email
+
+  
+        
+        # decode the data
+        
+        
+        # if ordersCollection.
+        #   ordersCollection.insert_one({"OrderNumber": orderNumber, "Total": total, "Subtotal": subtotal, "Shipping": shipping})
+        # else:
+        #   print("Order already exists")
+          #myDict = {"Total": total, "Subtotal": subtotal, "Shipping": shipping}
+          #print(myDict)
           
 
         
